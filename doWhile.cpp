72477d8b36c12796759f73b5d4aef86f3b72cd37
Gilson Aureliano <gilson.aureliano22@gmail.com>
2021-09-17 12:20:21 -0300

    doWhile

---
+++ doWhile.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	 int num;
+	 do{
+	 	printf("Digite um numero: ");
+	 	scanf(" %d", &num);
+	 	if (num != 0){
+	 		printf("O numero = %d \n", num);
+		 }
+	 	
+	 	
+	 }while (num != 0);
+	 
+	 return 0;
+	
+	
+	
+}
